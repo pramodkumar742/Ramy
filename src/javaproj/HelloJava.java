66e32b3ee328d3d03f7c63457411d491d5496ae3
Vivaan <Vivaan@DESKTOP-TESF9V8>
2023-12-10 18:54:55 +0530

    Commit Master

--- src/javaproj/HelloJava.java
+++ src/javaproj/HelloJava.java
@@ -5,6 +5,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("HelloJava");
 		System.out.println("This is Java Program");
+		System.out.println("Hi");
 	}
 
 }
